010edaaf8d00501539673aa1c1f9d5da94b307d0
FakeStandard <rileylin.space@gmail.com>
2021-10-16 13:42:49 +0800

    create MathLib library

---
+++ Chapter2-CoreCSharp/MathLib/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLib
+{
+    class Calculator
+    {
+        public static double Add(double x, double y) => x + y;
+        public static double Subtract(double x, double y) => x - y;
+    }
+}
