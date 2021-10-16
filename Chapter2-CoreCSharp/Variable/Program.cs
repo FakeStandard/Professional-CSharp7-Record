8d3b49acd0156a747f884e82ed6c2f6286d479d5
FakeStandard <rileylin.space@gmail.com>
2021-10-16 13:31:09 +0800

    create Variable project

---
+++ Chapter2-CoreCSharp/Variable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 類別推斷
+            var name = "Riley Lin";
+            var age = 18;
+            var isStudent = false;
+
+            Type nameType = name.GetType();
+            Type ageType = age.GetType();
+            Type isStudentType = isStudent.GetType();
+
+            Console.WriteLine($"name is of type {nameType}");
+            Console.WriteLine($"age is of type {ageType}");
+            Console.WriteLine($"isStudent is of type {isStudentType}");
+        }
+    }
+}
