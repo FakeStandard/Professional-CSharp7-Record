a05ee5f56d98ef4d8e9b3b8f8b3c56282ae0dd35
FakeStandard <rileylin.space@gmail.com>
2021-10-16 13:53:23 +0800

    create NamespaceSample project

---
+++ Chapter2-CoreCSharp/NamespaceSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// 賦予別名
+using Introduction = CSharpTest.Basics;
+
+namespace NamespaceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // :: 命名空間修飾符
+            Introduction::NamespaceExample NE = new Introduction::NamespaceExample();
+            Console.WriteLine(NE.GetNamespace());
+        }
+    }
+}
+
+namespace CSharpTest.Basics
+{
+    class NamespaceExample
+    {
+        public string GetNamespace() => GetType().Namespace;
+    }
+}
