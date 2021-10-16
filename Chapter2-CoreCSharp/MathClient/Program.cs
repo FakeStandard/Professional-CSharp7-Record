04b3d0a8c653c3a6237e61604c0e1445c0d59f79
FakeStandard <rileylin.space@gmail.com>
2021-10-16 13:45:47 +0800

    create MathClient project

---
+++ Chapter2-CoreCSharp/MathClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MathLib;
+
+namespace MathClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d1 = 3;
+            double d2 = 4;
+            double result = Calculator.Add(d1, d2);
+            Console.WriteLine(result);
+        }
+    }
+}
--- Chapter2-CoreCSharp/MathLib/Calculator.cs
+++ Chapter2-CoreCSharp/MathLib/Calculator.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace MathLib
 {
-    class Calculator
+    public class Calculator
     {
         public static double Add(double x, double y) => x + y;
         public static double Subtract(double x, double y) => x - y;
