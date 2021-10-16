43d41380979be2272cfc52af2a4e8e09b3f74383
FakeStandard <rileylin.space@gmail.com>
2021-10-16 13:40:20 +0800

    create VariableScope project

---
+++ Chapter2-CoreCSharp/VariableScope/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VariableScope
+{
+    class Program
+    {
+        static int k = 10;
+        static void Main(string[] args)
+        {
+            // Scope 1
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(i);
+            for (int i = 9; i >= 0; i--)
+                Console.WriteLine(i);
+
+            // Scope 2
+            int j = 10;
+            for(int i = 0; i < 10; i++)
+            {
+                // j is still in scope, so can't do this.
+                // int j = 20;
+                Console.WriteLine(i);
+            }
+
+            // Scope 3
+            int k = 20;
+            Console.WriteLine(k);
+            Console.WriteLine(Program.k);
+        }
+    }
+}
