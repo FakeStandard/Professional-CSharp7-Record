be7c1fc5b091d56637da0649b1f5e2ff494613df
FakeStandard <rileylin.space@gmail.com>
2021-10-19 23:05:57 +0800

    create ReadonlyStruct project

---
+++ Chapter3-ObjectsAndTypes/ReadonlyStruct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReadonlyStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Dimensions(3, 6);
+            Foo(point);
+
+            Console.ReadLine();
+        }
+
+        public static void Foo(Dimensions dimensions)
+        {
+
+        }
+    }
+}
