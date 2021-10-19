7fe213b193064c5138805d3725c5786d7026c944
FakeStandard <rileylin.space@gmail.com>
2021-10-19 17:23:39 +0800

    create ClassesSample project

---
+++ Chapter3-ObjectsAndTypes/ClassesSample/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesSample
+{
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ Chapter3-ObjectsAndTypes/ClassesSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassesSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Lin", "Riley");
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+            Console.WriteLine(person.FullName);
+        }
+    }
+}
