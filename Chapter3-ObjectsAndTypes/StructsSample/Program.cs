993dcb7249dfd67fd05cf7d4811f338358927dfd
FakeStandard <rileylin.space@gmail.com>
2021-10-19 23:03:48 +0800

    create StructSample project

---
+++ Chapter3-ObjectsAndTypes/StructsSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StructsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Dimensions(3, 6);
+        }
+    }
+}
