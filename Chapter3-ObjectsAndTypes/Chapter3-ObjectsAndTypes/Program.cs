4b08d74c382bdd863cf483daf49a740a1ced015e
FakeStandard <rileylin.space@gmail.com>
2021-10-19 11:47:13 +0800

    create chapter3 project

---
+++ Chapter3-ObjectsAndTypes/Chapter3-ObjectsAndTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter3_ObjectsAndTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
