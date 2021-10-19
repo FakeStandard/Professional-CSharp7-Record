7e4b9d2cd338be43a2e9a0392e79eb214dd320ee
FakeStandard <rileylin.space@gmail.com>
2021-10-19 17:18:58 +0800

    create MethodsSample project

---
+++ Chapter3-ObjectsAndTypes/MethodsSample/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsSample
+{
+    public class Math
+    {
+        public int Value { get; set; }
+
+        public int GetSquare() => Value * Value;
+        public static int GetSquareOf(int x) => x * x;
+        public static double GetPi() => 3.14159;
+    }
+}
---
+++ Chapter3-ObjectsAndTypes/MethodsSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Try calling some static functions.
+            Console.WriteLine($"Pi is {Math.GetPi()}");
+
+            int x = Math.GetSquareOf(5);
+            Console.WriteLine($"Square of 5 is {x}");
+
+            // Instantiate a Math object
+            // Instantiate a reference type.
+            var math = new Math();
+            math.Value = 30;
+            Console.WriteLine($"Value field of math variable contains {math.Value}");
+            Console.WriteLine($"Square of 30 is {math.GetSquare()}");
+        }
+    }
+}
