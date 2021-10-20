b2ac173f2037718378fd196c23788147c1e42230
FakeStandard <rileylin.space@gmail.com>
2021-10-20 13:53:37 +0800

    create PassingByValueReference project

---
+++ Chapter3-ObjectsAndTypes/PassingByValueAndReference/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PassingByValueAndReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new A { X = 1 };
+            ChangeA(a1);
+            Console.WriteLine(a1.X);
+
+            B b1 = new B { X = 1 };
+            ChangeB(b1);
+            Console.WriteLine(b1.X);
+        }
+
+        static void ChangeA(A a)
+        {
+            a.X = 2;
+        }
+
+        static void ChangeB(B b)
+        {
+            b.X = 2;
+        }
+    }
+
+    struct A
+    {
+        public int X { get; set; }
+    }
+
+    class B
+    {
+        public int X { get; set; }
+    }
+}
