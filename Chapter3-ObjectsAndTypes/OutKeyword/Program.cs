6ec875e3c71012f980368a38e94e5a3d99e64cc6
FakeStandard <rileylin.space@gmail.com>
2021-10-20 15:15:24 +0800

    create OutKeyword project

---
+++ Chapter3-ObjectsAndTypes/OutKeyword/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OutKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // version 1
+            string input1 = Console.ReadLine();
+            int result1 = int.Parse(input1);
+            Console.WriteLine(result1);
+
+            // version 2
+            string input2 = Console.ReadLine();
+
+            if (int.TryParse(input2, out int result))
+                Console.WriteLine(input2);
+            else
+                Console.WriteLine("not a number");
+
+            // vsersion 3
+            string input3 = Console.ReadLine();
+            Console.WriteLine(GetParseResult(input3));
+        }
+
+        static string GetParseResult(string input) =>
+            int.TryParse(input, out int result) ? $"n: {result}" : "not a number";
+    }
+}
