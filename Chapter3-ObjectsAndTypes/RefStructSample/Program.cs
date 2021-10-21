0eaa5590975b66f70a6c9e3c8e3ea9d5aec1a6e7
FakeStandard <rileylin.space@gmail.com>
2021-10-21 15:05:29 +0800

    create RefStructSample project

---
+++ Chapter3-ObjectsAndTypes/RefStructSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RefStructSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueTypeOnly vt = new ValueTypeOnly(10);
+            vt.AMethod();
+        }
+    }
+}
